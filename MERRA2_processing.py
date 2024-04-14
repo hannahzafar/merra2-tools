55841319151446c26c7bb5d7f31ca4864572e411
Hannah Zafar <hannah.zafar@nasa.gov>
2024-04-14 15:48:01 -0400

    Initial commit

---
+++ MERRA2_processing.py
@@ -0,0 +1,140 @@
+#! /usr/bin/env python
+#This code requires the following argument ['N' or 'S']
+
+import numpy as np
+import numpy.ma as ma
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import xarray as xr
+import netCDF4
+import datetime
+import time
+import sys
+
+POLE=sys.argv[1]
+DIR = '/discover/nobackup/hzafar/MERRA2'
+years = np.arange(1990,2011,1)
+months = np.arange(1,13)
+
+'''
+####### SLP ####################################
+var_name = 'SLP'
+vars = [var_name]
+############# ALL MONTHS
+flist = []
+for year in years:
+  for mon in months:
+    mon_str=str(mon)
+    if mon < 10: 
+        mon_str ='0'+mon_str
+    fname = DIR+'/Y'+str(year)+'/M'+mon_str+'/MERRA2.tavgM_2d_slv_Nx.'+str(year)+mon_str+'.nc4'
+    flist.append(fname)
+
+ds_MERRA2 = xr.open_mfdataset(flist,parallel=True, chunks = {'time':10})[vars]
+if POLE == 'N':
+    ds_MERRA2 = ds_MERRA2.where(ds_MERRA2.lat>=60,drop=True).mean(dim=["lat","lon"])
+else:
+    ds_MERRA2 = ds_MERRA2.where(ds_MERRA2.lat<=-60,drop=True).mean(dim=["lat","lon"])
+
+ds_MERRA2 = ds_MERRA2.to_dataframe()
+ds_MERRA2.index = ds_MERRA2.index.normalize()
+
+filename = 'MERRA2_extract_'+ var_name + '_' + POLE  + '.csv'
+path = 'transfer/'+filename
+ds_MERRA2.to_csv(path)
+print('Dataset written to .csv file')
+
+########### SEASONAL MAX/MIN ONLY (OLD)
+#if POLE == 'N':
+#  SEASON=['M03', 'M09']
+#  MONTH=['MAR', 'SEP']
+#else:
+#  SEASON=['M09', 'M02']
+#  MONTH=['SEP', 'FEB']
+#
+#flist = []
+#for year in years:
+#  for i, (sea,mon) in enumerate(zip(SEASON,MONTH)):
+#    fname = DIR+'/Y'+str(year)+'/'+sea+'/MERRA2.tavgM_2d_slv_Nx.'+str(year)+sea[-2:]+'.nc4'
+#    flist.append(fname)
+#
+#ds_MERRA2 = xr.open_mfdataset(flist,parallel=True)[vars]
+#    
+#if POLE == 'N':
+#    ds_MERRA2 = ds_MERRA2.where(ds_MERRA2.lat>=60,drop=True).mean(dim=["lat","lon"])
+#else:
+#    ds_MERRA2 = ds_MERRA2.where(ds_MERRA2.lat<=-60,drop=True).mean(dim=["lat","lon"])
+#
+#ds_MERRA2 = ds_MERRA2.to_dataframe()
+#filename = 'MERRA2_extract_sea'+ var_name + '_' + POLE  + '.csv'
+#path = 'transfer/'+filename
+#ds_MERRA2.to_csv(path)
+#print('Dataset written to .csv file')
+'''
+
+'''
+############ SNOWFALL############
+var_name = 'PRECSNO'
+vars = [var_name]
+
+############ ALL MONTHS
+flist = []
+for year in years:
+  for mon in months:
+    mon_str=str(mon)
+    if mon < 10:
+        mon_str ='0'+mon_str
+    fname = DIR+'/Y'+str(year)+'/M'+mon_str+'/MERRA2.tavgM_2d_flx_Nx.'+str(year)+mon_str+'.nc4'
+    flist.append(fname)
+
+ds_MERRA2 = xr.open_mfdataset(flist,parallel=True)[vars]
+    
+if POLE == 'N':
+    ds_MERRA2 = ds_MERRA2.where(ds_MERRA2.lat>=60,drop=True).mean(dim=["lat","lon"])
+else:
+    ds_MERRA2 = ds_MERRA2.where(ds_MERRA2.lat<=-60,drop=True).mean(dim=["lat","lon"])
+
+ds_MERRA2 = ds_MERRA2.to_dataframe()
+ds_MERRA2.index = ds_MERRA2.index.normalize()
+
+filename = 'MERRA2_extract_'+ var_name + '_' + POLE  + '.csv'
+path = 'transfer/'+filename
+ds_MERRA2.to_csv(path)
+print('Dataset written to .csv file')
+'''
+
+
+
+
+#'''
+############ SKIN TEMP###########
+var_name = 'TS'
+vars = [var_name]
+
+############ ALL MONTHS
+flist = []
+for year in years:
+  for mon in months:
+    mon_str=str(mon)
+    if mon < 10:
+        mon_str ='0'+mon_str
+    fname = DIR+'/Y'+str(year)+'/M'+mon_str+'/MERRA2.tavgM_2d_slv_Nx.'+str(year)+mon_str+'.nc4'
+    flist.append(fname)
+###print(flist)
+ds_MERRA2 = xr.open_mfdataset(flist,parallel=True)[vars]
+    
+if POLE == 'N':
+    ds_MERRA2 = ds_MERRA2.where(ds_MERRA2.lat>=60,drop=True).mean(dim=["lat","lon"])
+else:
+    ds_MERRA2 = ds_MERRA2.where(ds_MERRA2.lat<=-60,drop=True).mean(dim=["lat","lon"])
+
+ds_MERRA2 = ds_MERRA2.to_dataframe()
+ds_MERRA2.index = ds_MERRA2.index.normalize()
+
+filename = 'MERRA2_extract_'+ var_name + '_' + POLE  + '.csv'
+path = 'transfer/'+filename
+ds_MERRA2.to_csv(path)
+print('Dataset written to .csv file')
+#'''
+
+
