6662f0cf10af3e820b84733edb81b570ce86c500
Hannah Zafar <hannah.zafar@nasa.gov>
2025-10-02 16:22:16 -0400

    Update SLP for if statement with args and modify find nc file workflow

--- MERRA2_processing.py
+++ MERRA2_processing.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 # Script used to process MERRA-2 data on discover for other analysis
 
-import numpy as np
 import xarray as xr
 import argparse
+import glob
 
 parser = argparse.ArgumentParser(description="User-specified parameters")
 polelist = ['N', 'S']
@@ -28,34 +28,30 @@ VAR = args.var
 DIR = '/discover/nobackup/hzafar/MERRA2_processing/MERRA2_all' # Made a new symlink to MERRA-2 data
 start_yr, end_yr = [1990, 2010]
 years = [str(year) for year in range(start_yr, end_yr+1)] 
-months_mm = [f"{i:02d}" for i in range(1, 13)]
+# months_mm = [f"{i:02d}" for i in range(1, 13)]
+
 
 ####### SLP ####################################
-var_name = 'SLP'
-vars = [var_name]
-############# ALL MONTHS
-flist = []
-for year in years:
-  for mon in months:
-    mon_str=str(mon)
-    if mon < 10: 
-        mon_str ='0'+mon_str
-    fname = DIR+'/Y'+str(year)+'/M'+mon_str+'/MERRA2.tavgM_2d_slv_Nx.'+str(year)+mon_str+'.nc4'
-    flist.append(fname)
+if VAR=='SLP':
+    flist = []
+    for year in years:
+        # for mon in months_mm:
+            # fname = DIR+'/Y'+str(year)+'/M' + mon +'/MERRA2.tavgM_2d_slv_Nx.'+str(year)+ mon +'.nc4' # Old way of finding only extact files
+        fnames = sorted(glob.glob( DIR+'/Y' + str(year) +'/M*/MERRA2.tavgM_2d_slv_Nx.*.nc4')) # Uses glob function, beware
+        flist= flist + fnames
 
-ds_MERRA2 = xr.open_mfdataset(flist,parallel=True, chunks = {'time':10})[vars]
-if POLE == 'N':
-    ds_MERRA2 = ds_MERRA2.where(ds_MERRA2.lat>=60,drop=True).mean(dim=["lat","lon"])
-else:
-    ds_MERRA2 = ds_MERRA2.where(ds_MERRA2.lat<=-60,drop=True).mean(dim=["lat","lon"])
+    ds_MERRA2 = xr.open_mfdataset(flist,parallel=True, chunks = {'time':10})[VAR]
+    if POLE == 'N':
+        ds_MERRA2 = ds_MERRA2.where(ds_MERRA2.lat>=60,drop=True).mean(dim=["lat","lon"])
+    else:
+        ds_MERRA2 = ds_MERRA2.where(ds_MERRA2.lat<=-60,drop=True).mean(dim=["lat","lon"])
 
-ds_MERRA2 = ds_MERRA2.to_dataframe()
-ds_MERRA2.index = ds_MERRA2.index.normalize()
-
-filename = 'MERRA2_extract_'+ var_name + '_' + POLE  + '.csv'
-path = 'transfer/'+filename
-ds_MERRA2.to_csv(path)
-print('Dataset written to .csv file')
+    ds_MERRA2 = ds_MERRA2.to_dataframe()
+    ds_MERRA2.index = ds_MERRA2.index.normalize() #This gets rid of the 30 min time stamp?
+    filename = 'MERRA2_extract_'+ VAR + '_' + POLE  + '.csv'
+    path = 'transfer/' + filename
+    ds_MERRA2.to_csv(path)
+    print('Dataset written to .csv file')
 
 ########### SEASONAL MAX/MIN ONLY (OLD)
 #if POLE == 'N':
