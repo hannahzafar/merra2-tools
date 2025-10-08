2123f4edf5fdca47664f91b11438a9d2215642db
Hannah Zafar <hannah.zafar@nasa.gov>
2025-10-08 11:58:16 -0400

    Sel only N + S America

--- MERRA2_processing_fluxnet.py
+++ MERRA2_processing_fluxnet.py
@@ -99,6 +99,17 @@ for year in years:
 
 ds_MERRA2 = xr.open_mfdataset(flist)[var]
 
+# Sel only N+S America
+americas_MERRA2 = ds_MERRA2.sel(
+    lon=slice(-170, -30),
+    lat=slice(-60,80)
+)
+
+print(americas_MERRA2)
+#FIX: doing the sel takes so long, I think I'm just gonna virtualize
+#FIX: Make this script the generation of the virtualizarr
+# create subfolders in this repo? One with my old code and one with this? Or fork my repo?
+
 # This is all old from prev code
 # ds_MERRA2 = ds_MERRA2.to_dataframe()
 # ds_MERRA2.index = ds_MERRA2.index.normalize() #This gets rid of the 30 min time stamp?
