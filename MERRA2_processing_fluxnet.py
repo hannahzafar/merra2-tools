79ed625bd8023f37d091224eb3f47e6289554f94
Hannah Zafar <hannah.zafar@nasa.gov>
2025-10-10 16:40:23 -0400

    Virtualize script, explore package option

--- MERRA2_processing_fluxnet.py
+++ MERRA2_processing_fluxnet.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python
+#FIX: Rename this script Virtualize merra data?
+# I would honestly love to make this a package (?) I could import elsewhere, that would be kind of cool
+
 # Script used to process MERRA-2 data for fluxnet analysis
 
 import xarray as xr
 import sys
 from MERRA2_query import find_MERRA2_files
+from virtualizarr import open_virtual_dataset
 
+#TODO: Turn these into path objects? Vzarr seems to use them?
 # Symlink to MERRA-2 data
 dir = '/discover/nobackup/hzafar/MERRA2_processing/MERRA2_all'
 
@@ -20,8 +25,18 @@ vars = varslist[0]
 fileslist = find_MERRA2_files(dir, freq1, freq2, group, str(start_yr), str(end_yr))
 # print(type(fileslist))
 # print(fileslist[-1])
-ds_MERRA2 = xr.open_dataset(fileslist[0])[vars]
+ds_MERRA2 = xr.open_mfdataset(fileslist[:10])[vars]
 # print(ds_MERRA2)
+
+#TODO: Run this test?
+virtual_datasets = [open_virtual_dataset(filepath, 
+                                         loadable_variables=["time"],
+                                         decode_times=True,
+                                         ) for filepath in fileslist]
+
+virtual_ds_combine = xr.concat(virtual_datasets, dim='time', coords='minimal',compat='override')
+#FIX: try parquet? instead of json
+virtual_ds_combine.virtualize.to_kerchunk('combined_test_loadable.json', format='json')
 
 '''
 ds_MERRA2 = xr.open_mfdataset(flist)[var]
