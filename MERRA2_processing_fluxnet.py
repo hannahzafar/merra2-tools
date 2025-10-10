0605398a0ea7f74dc5430749e17720297fcd6880
Hannah Zafar <hannah.zafar@nasa.gov>
2025-10-10 15:58:13 -0400

    Cleanup notes

--- MERRA2_processing_fluxnet.py
+++ MERRA2_processing_fluxnet.py
@@ -5,10 +5,11 @@ import xarray as xr
 import sys
 from MERRA2_query import find_MERRA2_files
 
-dir = '/discover/nobackup/hzafar/MERRA2_processing/MERRA2_all' # Made a new symlink to MERRA-2 data
+# Symlink to MERRA-2 data
+dir = '/discover/nobackup/hzafar/MERRA2_processing/MERRA2_all'
 
-#NOTE: How am I going to select years? If Amerflux varies? Similar to MiCASA I suppose, or should I look at all the years I have a record from and average across long time series??? 
-# AmeriFlux FLUXNET spans 1991-2021 across all the sites, individual sites vary, let's just start with thisk
+#NOTE: How am I going to select years, if Amerflux years varies across sites?
+# AmeriFlux FLUXNET spans 1991-2021 across all the sites, individual sites vary, let's just start with that
 start_yr, end_yr = [1991, 2021]
 freq1 = "tavg"
 freq2 = "M"
@@ -34,5 +35,4 @@ americas_MERRA2 = ds_MERRA2.sel(
 print(americas_MERRA2)
 #FIX: doing the sel takes so long, I think I'm just gonna virtualize
 #FIX: Make this script the generation of the virtualizarr
-# create subfolders in this repo? One with my old code and one with this? Or fork my repo?
 '''
