04ab4e5f6ae734d8eab5d698f6da718a007d1a60
Hannah Zafar <hannah.zafar@nasa.gov>
2025-10-15 16:37:45 -0400

    Open multifile list as virtual dataset

--- src/merra2_tools/virtualizarr.py
+++ src/merra2_tools/virtualizarr.py
@@ -1,23 +1,42 @@
 """ Insert description """
 import xarray as xr
-import sys
-from virtualizarr import open_virtual_dataset
+from obstore.store import LocalStore
+from virtualizarr import open_virtual_dataset, open_virtual_mfdataset
+from virtualizarr.parsers import HDFParser
+from virtualizarr.registry import ObjectStoreRegistry
+from pathlib import Path
 
-from .config import MERRA2_ROOT
-from .query import find_MERRA2_files
-
-ds_MERRA2 = xr.open_mfdataset(fileslist[:10])[vars]
-# print(ds_MERRA2)
+#TODO: Update description
+#TODO: Implement more checks it is a Path obj
 
 def create_vzarr_store(filepaths):
-virtual_datasets = [open_virtual_dataset(filepath, 
-                                         loadable_variables=["time"],
-                                         decode_times=True,
-                                         ) for filepath in fileslist]
+    # Build registry dict over all unique parent dirs in filepaths
+    registry_map = {}
+    for file in filepaths:
+        dir_path = file.parent
+        prefix = dir_path.as_uri() + "/"
+        if prefix not in registry_map:
+            registry_map[prefix] = LocalStore(prefix=dir_path)
 
-virtual_ds_combine = xr.concat(virtual_datasets, dim='time', coords='minimal',compat='override')
-#FIX: try parquet? instead of json
-virtual_ds_combine.virtualize.to_kerchunk('combined_test_loadable.json', format='json')
+    # This differs from VZarr2 documentation in that the filepath and the store are in separate directories: registers source dir as a file:// prefix and creates a LocalStore for that source directory 
+    registry = ObjectStoreRegistry(registry_map)
+
+    file_urls = [file.as_uri() for file in filepaths]
+
+    vds = open_virtual_mfdataset(
+        urls=file_urls,
+        parser=HDFParser(),
+        registry=registry,
+    )
+
+    return vds
+    #NOTE: So I have it able to open multiple files into a vds. How can I write that to a store? Do I want to try icechunk? (I was using json and parquet previously
+    # Is this process fast enough that I don't really need to "slice the data" (i.e., select only N+S America and vars I want) or is that still significant?
+
+
+    # This is old script for when I want to write to a store
+    # vstore_path = Path.cwd() /"virtual_store"
+    # vstore_path.mkdir(exist_ok=True)
 
 '''
 ds_MERRA2 = xr.open_mfdataset(flist)[var]
